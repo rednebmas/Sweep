0d21892c4516665403019de7743cc65f9204f03a
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2026-01-10 09:46:20 -0800

    Initial Commit

---
+++ Sweep/SweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweepApp.swift
+//  Sweep
+//
+//  Created by Sam Bender on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
